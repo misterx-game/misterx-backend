5ea0d75aea98938c64f2c2847130ba7c5c19d6e7
Lucas Bickel <hairmare@purplehaze.ch>
2015-11-15 16:33:34 +0100

    results of yo express

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'misterx-express'
+    },
+    port: 3001,
+    db: 'mongodb://localhost/misterx-express-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'misterx-express'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/misterx-express-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'misterx-express'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/misterx-express-production'
+  }
+};
+
+module.exports = config[env];
