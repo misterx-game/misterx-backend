3cd028a17f8f300c702364addddcbc56e4961c50
mathias petermann <mathias.petermann@unic.com>
2016-01-18 21:53:04 +0100

    added game endpoint

---
+++ app/endpoints/game.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+  cors = require('cors'),
+  passport = require('passport'),
+  mongoose = require('mongoose'),
+  restify = require('express-restify-mongoose'),
+  mustbe = require("mustbe").routeHelpers(),
+  router = express.Router(),
+  Game = mongoose.model('Game');
+
+module.exports = function (app) {
+  app.use(cors());
+  app.use(passport.authenticate('jwt', { session: false }));
+  app.use('/', router);
+
+  okAuth = function(req, res, next) {
+    next();
+  };
+  noAuth = function(req, res, next) {
+    res.send(401, 'Authorization failed');
+  };
+
+  restify.serve(router, Game, {
+    version: '',
+    prefix: '',
+    lowercase: true,
+    preCreate: [
+      mustbe.authorized('admin games', okAuth, noAuth),
+      function(req, res, next) {
+        if (new Date(req.body.start) > new Date(req.body.end)) {
+          res.send(400, 'Enddate must be after Startdate');
+        }
+        next();
+      }
+    ],
+    preUpdate: [
+      mustbe.authorized('admin games', okAuth, noAuth)
+    ],
+    preDelete: mustbe.authorized('admin games', okAuth, noAuth)
+  });
+};
+
