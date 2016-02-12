c8dcd129386a24f75dbd11516376edf545114a52
Lukas Grimm <grimm@puzzle.ch>
2016-02-12 15:18:36 +0100

    added config

--- config/config.js
+++ config/config.js
@@ -19,13 +19,20 @@ var config = {
         // clientId: '',
         clientId: 'f368e86456d2bf10b466',
         // clientSecret: ''
-        clientSecret: '07a595f9b07268a8fd324c0cb63ae7ef3d39dda2'
+        clientSecret: ''
       },
       admin: {
         // clientId: '',
         clientId: 'd03b234ae7ea7d56bc6e',
         // clientSecret: ''
-        clientSecret: 'd2e5403fa7825b2bbc06d571873e84649048e934'
+        clientSecret: ''
+      }
+    },
+    facebook: {
+      client: {
+        clientId: '1691596147748247',
+        clientSecret: '',
+        callbackURL: 'http://localhost:3000/auth/facebook'
       }
     },
     cors: {
